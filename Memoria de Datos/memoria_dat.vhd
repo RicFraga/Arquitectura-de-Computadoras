d2d7822d13557ca302e9f03f8379341043bb95f3
RicFraga <40702885+RicFraga@users.noreply.github.com>
2020-12-02 11:01:15 -0600

    parametrización

--- Memoria de Datos/memoria_dat.vhd
+++ Memoria de Datos/memoria_dat.vhd
@@ -5,6 +5,10 @@ use IEEE.STD_LOGIC_arith.ALL;
 use IEEE.STD_LOGIC_unsigned.ALL;
 
 entity memoria_dat is
+    generic (
+        m : integer := 11;
+        n : integer := 16
+    );
     Port ( dir : in STD_LOGIC_VECTOR (10 downto 0);
            dataIn : in STD_LOGIC_VECTOR (15 downto 0);
            WD : in STD_LOGIC;
@@ -14,7 +18,7 @@ end memoria_dat;
 
 architecture Behavioral of memoria_dat is
 
-type matriz IS ARRAY(0 to 2047) OF STD_LOGIC_VECTOR(15 downto 0);
+type matriz IS ARRAY(0 to ((2**m) - 1)) OF STD_LOGIC_VECTOR(n - 1 downto 0);
 --signal memoria : matriz := (others => x"0000");
 signal memoria: matriz;
 
@@ -22,11 +26,12 @@ begin
 
 process(clk)
 begin
+    
     if(rising_edge(clk) and WD = '1') then
         memoria(conv_integer(dir)) <= dataIn;
         
-    end if;   
-end process;
-dataOut <= memoria(conv_integer(dir));
+    end if;    
+    end process;
+    dataOut <= memoria(conv_integer(dir));
 
 end Behavioral;
